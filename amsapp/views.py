2147792378edca2aa8413459b499cde1e5b5e5c8
usif mohamed <yousifm836@gmail.com>
2022-08-29 11:12:43 +0200

    app configration done starting in templates

--- amsapp/views.py
+++ amsapp/views.py
@@ -1,3 +1,15 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def home(request):
+    return HttpResponse("hello there from home ")
+
+
+
+def products(request):
+    return HttpResponse("hello there from products ")
+
+def customer(request):
+    return HttpResponse("hello there from customer ")
