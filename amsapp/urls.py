a1f84f8c00324ccdf4ef2f4f6a4ab32d7dc4dd17
usif mohamed <yousifm836@gmail.com>
2022-09-02 12:55:58 +0200

    registration and login using django UserCreaionForm

--- amsapp/urls.py
+++ amsapp/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('',views.home , name="home"),
+    path('register/',views.registerPage, name="register"),
+    path('login/',views.loginPage , name="login"),
+    path('logout/',views.logoutUser , name="logout"),
     path('product/',views.products , name="products"),
     path('customer/<str:id>',views.customer , name="customer" ),
     path('order/<str:id>',views.create_order , name="order"),
--- amsapp/views.py
+++ amsapp/views.py
@@ -3,12 +3,58 @@ from multiprocessing import context
 from django.shortcuts import render ,redirect
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate ,login,logout
+from django.contrib.auth.decorators import login_required
 
 from .models import *  
 from .form import *  
 from .filters import OrderFilter
 # Create your views here.
 
+def registerPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:    
+        form = CreateUserForm()
+        if request.method =="POST":
+            form = CreateUserForm(request.POST)
+            print(form.data)
+            print(form.is_valid())
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request,"Account was created for " +user)
+                return redirect("login")
+        context={"form":form,}
+        return render(request,"accounts/register.html",context)
+
+
+
+def loginPage(request):
+     if request.user.is_authenticated:
+        return redirect('home')
+     else :   
+        if request.method=="POST":
+            username= request.POST.get('username')
+            password=request.POST.get('password')
+            user=authenticate(request,username=username,password=password)
+            if user is not None:
+                    login(request,user)
+                    return redirect('/')
+        else:
+                messages.info(request,"username or password is in correct")    
+        context={}
+        return render(request,"accounts/login.html",context)
+
+def logoutUser(request):
+
+    logout(request)
+    return redirect('login')
+
+
+@login_required(login_url='login')
 def home(request):
     orders = Order.objects.all()
     customers = Customer.objects.all()
@@ -26,14 +72,14 @@ def home(request):
     }
     return render(request , 'accounts/dashboard.html',context)
 
-
+@login_required(login_url='login')
 def products(request):
     products = Product.objects.all()
     context={
         "products" : products,
     }
     return render(request , 'accounts/products.html' , context)
-
+@login_required(login_url='login')
 def customer(request , id):
     cus = Customer.objects.get(id=id)
     orders = cus.order_set.all()
@@ -50,7 +96,7 @@ def customer(request , id):
     }
     return render(request , 'accounts/customers.html',context)
 
-
+@login_required(login_url='login')
 def create_order(request,id):  
     OrderFormSet = inlineformset_factory(Customer,Order,fields=('product','status'))  
     customer = Customer.objects.get(id=id)
@@ -67,7 +113,7 @@ def create_order(request,id):
         "customer":customer,
     }    
     return render(request,'accounts/order_form.html',context)    
-
+@login_required(login_url='login')
 def update_order(request,id):
     order = Order.objects.get(id=id)
     form = OrderForm(instance=order)
@@ -80,7 +126,7 @@ def update_order(request,id):
         "form":form,
     }
     return render(request,'accounts/order_form.html',context)
-
+@login_required(login_url='login')
 def delete_order(request,id):
     order = Order.objects.get(id=id)
     if request.method == "POST":
