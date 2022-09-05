f3101d641085f90d041b186416ce8534125b5955
usif mohamed <yousifm836@gmail.com>
2022-09-05 12:31:45 +0200

    userr profile created with adding user to customer group by default

--- amsapp/urls.py
+++ amsapp/urls.py
@@ -3,10 +3,16 @@ from . import views
 
 urlpatterns = [
     path('',views.home , name="home"),
+    
+    
     path('register/',views.registerPage, name="register"),
     path('login/', views.loginPage , name="login"),
     path('logout/',views.logoutUser , name="logout"),
+
+
     path('user/', views.userPage , name="user-page"),
+    path('account/', views.accountSettings , name="accounts"),
+    
     path('product/',views.products , name="products"),
     path('customer/<str:id>',views.customer , name="customer" ),
     path('order/<str:id>',views.create_order , name="order"),
--- amsapp/views.py
+++ amsapp/views.py
@@ -3,7 +3,7 @@ from multiprocessing import context
 from django.shortcuts import render ,redirect
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm 
 from django.contrib import messages
 from django.contrib.auth import authenticate ,login,logout
 from django.contrib.auth.decorators import login_required
@@ -96,6 +96,23 @@ def userPage(request):
 
     }
     return render(request,'accounts/user.html',context)
+
+
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['customer'])
+def accountSettings(request):
+    customer = request.user.customer
+    form = CustomerForm(instance=customer)
+
+    if request.method == "POST":
+        form = CustomerForm(request.POST,request.FILES , instance=customer )
+        if form.is_valid():
+            form.save()
+
+    context={
+        "form":form,
+    }
+    return render(request,'accounts/account_settings.html',context)
 
 @login_required(login_url='login')
 @allowed_users(allowed_roles=['admin'])
